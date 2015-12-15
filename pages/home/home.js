bea5ab42eac0ce2f8cbce116578c383d37fb937f
thanhkhang2312 <khanglethanh@Khangs-MacBook-Pro.local>
2015-12-15 14:21:33 +0200

    render title from product.son

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,8 @@
 (function(){
 	angular.module('OnlineStore')
 		.controller('HomeController',['$http','$scope',function($http,$scope){
+			$http.get('./json/product.json').success(function(data){
+				$scope.products = data;
+			});
 		}]);
 })();
