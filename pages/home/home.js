ac93c3278314dca80d4eb2d52b8d1d8ec56f153b
thanhkhang2312 <khanglethanh@Khangs-MacBook-Pro.local>
2015-12-15 15:49:34 +0200

    edit content

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,6 @@
 (function(){
 	angular.module('OnlineStore')
 		.controller('HomeController',['$http','$scope',function($http,$scope){
-<<<<<<< HEAD
 
 			$http.get('./json/swiper.json').success(function(response){
 				$scope.swipers = response;
@@ -12,14 +11,7 @@
 			$http.get('./json/product.json').success(function(data){
 				$scope.products = data;
 			
-=======
-			$http.get('./json/swiper.json').success(function(data){
-				$scope.swipers = data;
-			});	
-			$scope.myInterval = 3000;
-			$http.get('./json/product.json').success(function(data){
-				$scope.products = data;
->>>>>>> origin/master
+
 			});
 		}]);
 })();
