cc37caaf2f573ea5997d32f2eccff11c2fb24f38
Hung Pham <thanhhung.phamquang@gmail.com>
2015-12-15 15:50:14 +0200

    update
    update

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,7 @@
 (function(){
 	angular.module('OnlineStore')
 		.controller('HomeController',['$http','$scope',function($http,$scope){
-<<<<<<< HEAD
+
 
 			$http.get('./json/swiper.json').success(function(response){
 				$scope.swipers = response;
@@ -11,15 +11,7 @@
 
 			$http.get('./json/product.json').success(function(data){
 				$scope.products = data;
-			
-=======
-			$http.get('./json/swiper.json').success(function(data){
-				$scope.swipers = data;
-			});	
-			$scope.myInterval = 3000;
-			$http.get('./json/product.json').success(function(data){
-				$scope.products = data;
->>>>>>> origin/master
 			});
+
 		}]);
 })();
