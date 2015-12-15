32dd19fd3dc688eadfb3322f09728739f5fde24c
thanhkhang2312 <khanglethanh@Khangs-MacBook-Pro.local>
2015-12-15 15:41:36 +0200

    render image tv, mobile, tablet for home page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,14 +1,12 @@
 (function(){
 	angular.module('OnlineStore')
 		.controller('HomeController',['$http','$scope',function($http,$scope){
-<<<<<<< HEAD
-			$scope.myInterval = 3000;
 			$http.get('./json/swiper.json').success(function(data){
 				$scope.swipers = data;
-=======
+			});	
+			$scope.myInterval = 3000;
 			$http.get('./json/product.json').success(function(data){
 				$scope.products = data;
->>>>>>> origin/master
 			});
 		}]);
 })();
