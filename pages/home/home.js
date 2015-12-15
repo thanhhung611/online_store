8e8c0ed14c3971a50934118e3c85d7f50aeb83da
Hung Pham <thanhhung.phamquang@gmail.com>
2015-12-15 15:42:23 +0200

    slider successfully, ghien
    slider successfully, ghien

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,14 +1,16 @@
 (function(){
 	angular.module('OnlineStore')
 		.controller('HomeController',['$http','$scope',function($http,$scope){
-<<<<<<< HEAD
+
+			$http.get('./json/swiper.json').success(function(response){
+				$scope.swipers = response;
+				
+			});
 			$scope.myInterval = 3000;
-			$http.get('./json/swiper.json').success(function(data){
-				$scope.swipers = data;
-=======
+
 			$http.get('./json/product.json').success(function(data){
 				$scope.products = data;
->>>>>>> origin/master
+			
 			});
 		}]);
 })();
